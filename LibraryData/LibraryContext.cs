751bb4d3891a7b5b5662688e1bd2107d7c55b139
Mano Cardozo <manaus_pc@hotmail.com>
2019-08-24 21:44:01 +1200

    initial commit

---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        DbSet<Book> Books { get; set; }
+        DbSet<Video> Videos { get; set; }
+        DbSet<Checkout> Checkouts { get; set; }
+        DbSet<CheckoutHistory> CheckoutHistories { get; set; }
+        DbSet<LibraryBranch> LibraryBranches { get; set; }
+        DbSet<BranchHours> BranchHours { get; set; }
+        DbSet<LibraryCard> LibraryCards { get; set; }
+        DbSet<Patron> Patrons { get; set; }
+        DbSet<Status> Statuses { get; set; }
+        DbSet<LibraryAsset> LibraryAssets { get; set; }
+        DbSet<Hold> Holds { get; set; }
+    }
+}
