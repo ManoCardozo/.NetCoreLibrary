b534b576f9a1c766bf666bb8e028c9622280fff5
Mano Cardozo <manaus_pc@hotmail.com>
2019-08-25 20:16:18 +1200

    Added branch service

---
+++ Library/Controllers/BranchController.cs
@@ -0,0 +1,59 @@
+﻿using Library.Models.Branch;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Library.Controllers
+{
+    public class BranchController : Controller
+    {
+        private ILibraryBranch _branch;
+
+        public BranchController(ILibraryBranch branch)
+        {
+            _branch = branch;
+        }
+
+        public IActionResult Index()
+        {
+            var branches = _branch
+                .GetAll()
+                .Select(b => new BranchDetailModel
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                    IsOpen = _branch.IsBranchOpen(b.Id),
+                    NumberOfAssets = _branch.GetAssets(b.Id).Count(),
+                    NumberOfPatrons = _branch.GetPatrons(b.Id).Count(),
+                });
+
+            var model = new BranchIndexModel
+            {
+                Branches = branches
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var branch = _branch.Get(id);
+
+            var model = new BranchDetailModel
+            {
+                Id = branch.Id,
+                Name = branch.Name,
+                Address = branch.Address,
+                Telephone = branch.Telephone,
+                OpenDate = branch.OpenDate.ToString("yyyy-MM-dd"),
+                NumberOfAssets = _branch.GetAssets(id).Count(),
+                NumberOfPatrons = _branch.GetPatrons(id).Count(),
+                TotalAssetValue = _branch.GetAssets(id).Sum(a => a.Cost),
+                ImageUrl = branch.ImageUrl,
+                HoursOpen = _branch.GetBranchHours(id)
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ LibraryServices/DataHelpers.cs
@@ -0,0 +1,39 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryServices
+{
+    public class DataHelpers
+    {
+        public static IEnumerable<string> HumanizeBuzHours(IEnumerable<BranchHours> branchHours)
+        {
+            var hours = new List<string>();
+
+            foreach (var time in branchHours)
+            {
+                var day = HumanizeDay(time.DayOfWeek);
+                var openTime = HumanizeTime(time.OpenTime);
+                var closeTime = HumanizeTime(time.CloseTime);
+
+                var timeEntry = $"{day} {openTime} to {closeTime}";
+
+                hours.Add(timeEntry);
+            }
+
+            return hours;
+        }
+
+        private static string HumanizeDay(int number)
+        {
+            // our data correlates 1 -> Sunday, so subtract 1
+            return Enum.GetName(typeof(DayOfWeek), number - 1);
+        }
+
+        private static string HumanizeTime(int time)
+        {
+            var result = TimeSpan.FromHours(time);
+            return result.ToString("hh':'mm");
+        }
+    }
+}
